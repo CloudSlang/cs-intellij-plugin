402465a8d3f92d41237498e664894dcede2a2053
Lucian Musca <lucian-cristian.musca@hpe.com>
2016-09-23 11:39:45 +0300

    Remove unused macro

--- src/com/intellij/lang/cloudslang/completion/macro/CurrentNamespaceMacro.java
+++ src/com/intellij/lang/cloudslang/completion/macro/CurrentNamespaceMacro.java
@@ -5,25 +5,38 @@ import com.intellij.codeInsight.template.ExpressionContext;
 import com.intellij.codeInsight.template.Macro;
 import com.intellij.codeInsight.template.Result;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.codeInsight.template.TextResult;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 
 public class CurrentNamespaceMacro extends Macro {
+
+    private static final String DEFAULT_NAMESPACE_TO_USE = "io.cloudslang.content";
+    private static final String RELATIVE_PATH_TO_PROJECT = "relativePathToProject";
+
     @Override
     public String getName() {
-        return "currentNamespace";
+        return RELATIVE_PATH_TO_PROJECT;
     }
 
     @Override
     public String getPresentableName() {
-        return "currentNamespace";
+        return RELATIVE_PATH_TO_PROJECT;
     }
 
     @Override
     public Result calculateResult(@NotNull Expression[] params, ExpressionContext context) {
-//        Project project = context.getProject();
-//        PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
-//        file.getVirtualFile().getParent().getPath()
+        Project project = context.getProject();
+        if (context.getEditor() == null) {
+            return new TextResult(DEFAULT_NAMESPACE_TO_USE);
+        }
+        String basePath = project.getBasePath();
+        PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
+        String path = file.getVirtualFile().getPath();
+
         return null;
     }
 
