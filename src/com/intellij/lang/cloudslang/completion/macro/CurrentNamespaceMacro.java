280a6b155f0e04d4cadd5a717905d64713806cb8
Lucian Musca <lucian-cristian.musca@hpe.com>
2016-09-23 14:44:18 +0300

    Adding namespace calculator

--- src/com/intellij/lang/cloudslang/completion/macro/CurrentNamespaceMacro.java
+++ src/com/intellij/lang/cloudslang/completion/macro/CurrentNamespaceMacro.java
@@ -7,15 +7,24 @@ import com.intellij.codeInsight.template.Result;
 import com.intellij.codeInsight.template.TemplateContextType;
 import com.intellij.codeInsight.template.TextResult;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import static java.nio.file.Paths.get;
+import static java.util.Locale.ENGLISH;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 
 public class CurrentNamespaceMacro extends Macro {
 
     private static final String DEFAULT_NAMESPACE_TO_USE = "io.cloudslang.content";
     private static final String RELATIVE_PATH_TO_PROJECT = "relativePathToProject";
+    private static final String NAMESPACE_SEPARATOR = ".";
 
     @Override
     public String getName() {
@@ -24,7 +33,7 @@ public class CurrentNamespaceMacro extends Macro {
 
     @Override
     public String getPresentableName() {
-        return RELATIVE_PATH_TO_PROJECT;
+        return getName();
     }
 
     @Override
@@ -33,11 +42,16 @@ public class CurrentNamespaceMacro extends Macro {
         if (context.getEditor() == null) {
             return new TextResult(DEFAULT_NAMESPACE_TO_USE);
         }
-        String basePath = project.getBasePath();
-        PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
-        String path = file.getVirtualFile().getPath();
 
-        return null;
+        PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
+        if ((file == null) || (file.getVirtualFile() == null)) {
+            return new TextResult(DEFAULT_NAMESPACE_TO_USE);
+        }
+        VirtualFile virtualFile = file.getVirtualFile();
+        String editorFilePath = virtualFile.getPath();
+        String projectPath = project.getBasePath();
+
+        return new TextResult(fixNamespace(projectPath, editorFilePath));
     }
 
     @Override
@@ -49,4 +63,32 @@ public class CurrentNamespaceMacro extends Macro {
     public boolean isAcceptableInContext(TemplateContextType context) {
         return true;
     }
+
+    private String fixNamespace(final String projectPath, final String filePath) {
+        // Exclude file name from namespace value
+        Path relativePath = get(projectPath).relativize(get(new File(filePath).getParent()));
+        int nameCount = relativePath.getNameCount();
+        if (nameCount <= 0) {
+            return DEFAULT_NAMESPACE_TO_USE;
+        }
+        StringBuilder strBuilder = new StringBuilder(relativePath.toString().length());
+        int nameCountMinusOne = nameCount - 1;
+        for (int index = 0; index < nameCount; index++) {
+            String cleanPart = fixPathPart(relativePath.getName(index).toString());
+            if (isNotEmpty(cleanPart)) {
+                strBuilder.append(cleanPart.toLowerCase(ENGLISH)); // namespace should be lowercase
+                if (index < nameCountMinusOne) {
+                    strBuilder.append(NAMESPACE_SEPARATOR);
+                }
+            }
+        }
+        return strBuilder.toString();
+
+    }
+
+    private String fixPathPart(String pathPart) {
+        // We don't support dashes and spaces in files only letters, digits and underscores
+        return pathPart.replaceAll("\\s+", "_").replaceAll("[-]+", "_").replaceAll("[_]+", "_").replaceAll("\\W", "");
+    }
+
 }
