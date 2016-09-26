f8da9f33c4de1d7e75e154f648a2814c68fcb349
Lucian Musca <lucian-cristian.musca@hpe.com>
2016-09-26 13:36:19 +0300

    Fix issue with name not being correct in files not ending with just *.sl

--- src/com/intellij/lang/cloudslang/CloudSlangFileUtils.java
+++ src/com/intellij/lang/cloudslang/CloudSlangFileUtils.java
@@ -4,6 +4,9 @@ package com.intellij.lang.cloudslang;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.yaml.YAMLFileType;
 
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+import static org.apache.commons.lang3.StringUtils.endsWithIgnoreCase;
+
 public class CloudSlangFileUtils {
 
     private static final String[] CLOUD_SLANG_EXTENSIONS = {".sl", ".sl.yaml", ".sl.yml", ".prop.sl"};
@@ -12,19 +15,31 @@ public class CloudSlangFileUtils {
     }
 
     public static boolean isCloudSlangFile(PsiFile psiFile) {
-        if (psiFile.getFileType() != YAMLFileType.YML) {
-            return false;
-        }
-        return isCloudSlangFile(psiFile.getName());
+        return (psiFile.getFileType() == YAMLFileType.YML) && isCloudSlangFile(psiFile.getName());
     }
 
     public static boolean isCloudSlangFile(String fileName) {
         for (String extension : CLOUD_SLANG_EXTENSIONS) {
-            if (fileName.endsWith(extension)) {
+            if (endsWithIgnoreCase(fileName, extension)) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    public static String getNameWithoutExtension(final String fileName) {
+        // This code is to prevent order of array to affect this algorithm
+        if (endsWithIgnoreCase(fileName, CLOUD_SLANG_EXTENSIONS[3])) {
+            return fileName.substring(0, fileName.indexOf(CLOUD_SLANG_EXTENSIONS[3]));
+        } else if (endsWithIgnoreCase(fileName, CLOUD_SLANG_EXTENSIONS[0])) {
+            return fileName.substring(0, fileName.indexOf(CLOUD_SLANG_EXTENSIONS[0]));
+        } else if (endsWithIgnoreCase(fileName, CLOUD_SLANG_EXTENSIONS[1])) {
+            return fileName.substring(0, fileName.indexOf(CLOUD_SLANG_EXTENSIONS[1]));
+        } else if (endsWithIgnoreCase(fileName, CLOUD_SLANG_EXTENSIONS[2])) {
+            return fileName.substring(0, fileName.indexOf(CLOUD_SLANG_EXTENSIONS[2]));
+        } else {
+            return removeExtension(fileName);
+        }
     }
 }
---
+++ src/com/intellij/lang/cloudslang/completion/macro/CloudSlangExecutableNameMacro.java
@@ -0,0 +1,50 @@
+package com.intellij.lang.cloudslang.completion.macro;
+
+import com.intellij.codeInsight.template.Expression;
+import com.intellij.codeInsight.template.ExpressionContext;
+import com.intellij.codeInsight.template.Macro;
+import com.intellij.codeInsight.template.Result;
+import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.codeInsight.template.TextResult;
+import com.intellij.lang.cloudslang.CloudSlangFileUtils;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+
+public class CloudSlangExecutableNameMacro extends Macro {
+
+    @Override
+    public String getName() {
+        return "cloudSlangExecutableName";
+    }
+
+    @Override
+    public String getPresentableName() {
+        return "cloudSlangExecutableName()";
+    }
+
+    @Override
+    public Result calculateResult(@NotNull Expression[] params, ExpressionContext context) {
+        Project project = context.getProject();
+        if (context.getEditor() == null) {
+            return new TextResult("");
+        }
+        final PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
+        return (file == null) ? new TextResult("") :
+                new TextResult(CloudSlangFileUtils.getNameWithoutExtension(file.getName()));
+    }
+
+    @Override
+    public Result calculateQuickResult(@NotNull Expression[] params, ExpressionContext context) {
+        return calculateResult(params, context);
+    }
+
+    @Override
+    public boolean isAcceptableInContext(TemplateContextType context) {
+        return true;
+    }
+
+}
+
